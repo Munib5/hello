42a50018f666ce193748423f58867a1e6dfc6480
Munib5 <mm8625@nyu.edu>
2021-11-23 14:56:23 +0000

    final day 2 prac

--- ser/train.py
+++ ser/train.py
@@ -2,7 +2,9 @@ from pathlib import Path
 import torch
 from torch import optim
 from .model import Net
+from .model import MnistResNet
 import torch.nn.functional as F
+import torch.nn as nn
 
 from .data import train_data, val_data
 
@@ -10,10 +12,13 @@ import typer
 
 import json
 
+import git
+
+repo = git.Repo(search_parent_directories=True)
+sha = repo.head.object.hexsha
+
 PROJECT_ROOT = Path(__file__).parent.parent
 MODEL_DIR = PROJECT_ROOT / "model"
-
-print(MODEL_DIR)
 
 main = typer.Typer()
 
@@ -25,7 +30,7 @@ def train(name, learning_rate, epochs, batch_size):
     learning_rate = learning_rate
 
     # load model
-    model = Net().to(device)
+    model = MnistResNet().to(device)
 
     # setup params
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -37,6 +42,11 @@ def train(name, learning_rate, epochs, batch_size):
 
     print("Data loaded properly.")
 
+    val_loss_temp = 1e10
+    correct_temp = 0
+    epoch_temp = 0
+    loss_function = nn.CrossEntropyLoss()
+
     # train
     for epoch in range(epochs):
         for i, (images, labels) in enumerate(training_dataloader):
@@ -44,7 +54,8 @@ def train(name, learning_rate, epochs, batch_size):
             model.train()
             optimizer.zero_grad()
             output = model(images)
-            loss = F.nll_loss(output, labels)
+            #loss = F.nll_loss(output, labels)
+            loss = loss_function(output, labels)
             loss.backward()
             optimizer.step()
             print(
@@ -59,20 +70,27 @@ def train(name, learning_rate, epochs, batch_size):
                 images, labels = images.to(device), labels.to(device)
                 model.eval()
                 output = model(images)
-                val_loss += F.nll_loss(output, labels, reduction="sum").item()
+                #val_loss += F.nll_loss(output, labels, reduction="sum").item()
+                val_loss = loss_function(output, labels).item()
                 pred = output.argmax(dim=1, keepdim=True)
                 correct += pred.eq(labels.view_as(pred)).sum().item()
             val_loss /= len(validation_dataloader.dataset)
             val_acc = correct / len(validation_dataloader.dataset)
 
+            if val_loss < val_loss_temp:
+                val_loss_temp = val_loss
+            if val_acc > correct_temp:
+                correct_temp = val_acc
+                epoch_temp = i
+                print("Validation accuracy improved")
+                # save the parameters!
+                params = {'hash':sha, 'name':name, 'learning_rate':learning_rate, 'epochs':epochs, 'batch_size':batch_size, 'val_acc': val_acc, 'epoch': epoch}
+
+                with open(f'{name}.json','w') as d:
+                    json.dump(params, d)
+
+                torch.save(model.state_dict(), MODEL_DIR/f"model_{name}.pt")
+
             print(
                 f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
             )
-    # save the parameters!
-
-    params = {'name':name, 'learning_rate':learning_rate, 'epochs':epochs, 'batch_size':batch_size, 'val_acc': val_acc}
-
-    with open(f'{name}.json','w') as d:
-        json.dump(params, d)
-
-    torch.save(model.state_dict(), MODEL_DIR/f"model_{name}")
